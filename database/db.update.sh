5043986937ee204f6c7d99ebaea90d82efda2a95
Matt Travers <matt.travers@tcg.com>
2015-05-06 17:39:51 -0400

    Added liquibase files.

---
+++ database/db.update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Utility script to update changelogs against the database.
+#
+
+if [ "$#" -ne 2 ]; then
+	echo ""
+	echo "Error: You must enter the database username and password as arguments."
+	echo "Usage: ./db.update.sh dbUsername dbPassword"
+	echo ""
+	exit 1;
+fi
+
+./liquibase \
+	--changeLogFile=db.changelog-master.xml \
+	--username=$1 \
+	--password=$2 \
+	--url=jdbc:postgresql://localhost:5432/gsac \
+	--driver=org.postgresql.Driver \
+	--classpath=postgresql-9.4-1201.jdbc4.jar \
+	update
+
