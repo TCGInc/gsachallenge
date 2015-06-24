ada07b3bab2e20db15f4b36198f7fcad8111d7e0
Scott McCaughey <scott@shinybird.com>
2015-06-23 23:16:41 -0400

    Added legend for datamap colors.

--- public/js/ux.js
+++ public/js/ux.js
@@ -4,3 +4,21 @@ $(function() {
     trigger: 'hover'
   });
 });
+
+$(document).ready(function() {
+
+  var colorbrewerConfig = {
+    palette: "PuBu",
+    numberOfBands: 9
+  };
+
+  var fillKeys = [];
+  var fills = {};
+  for (var i = 0; i < colorbrewerConfig.numberOfBands; i++) {
+    console.log("band " + i);
+    console.log(colorbrewer[colorbrewerConfig.palette][colorbrewerConfig.numberOfBands][i]);
+  }
+
+  $('heatmap > div').append('<div class="map-leg"><div class="leg-less"><i class="fa fa-arrow-left"></i> Less Recalls</div><div class="leg-more">More Recalls <i class="fa fa-arrow-right"></i></div></div>');
+
+});
