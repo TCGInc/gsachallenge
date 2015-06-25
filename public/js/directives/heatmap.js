6bcae33ca2f90c21b5c843cce01d4b41bfecfa49
Scott McCaughey <scott@shinybird.com>
2015-06-25 14:24:28 -0400

    UI tweaks.

--- public/js/directives/heatmap.js
+++ public/js/directives/heatmap.js
@@ -34,7 +34,7 @@ app.directive("heatmap", function(utilityService) {
 		link: function(scope, element, attrs) {
 
 			// Declare getFillKey() function variable here for use across link functions.
-			// Function returns fill color band label and created in updateMap(). 
+			// Function returns fill color band label and created in updateMap().
 			var getFillKey;
 
 			// Initialize heatmap.
@@ -47,13 +47,13 @@ app.directive("heatmap", function(utilityService) {
 					borderWidth: 1,
 	        		borderColor: 'rgba(0,0,0,.5)',
 			        popupTemplate: function(geo, data) {
-			            return ['<div class="hoverinfo"><strong>',
-			                    'Number of events in ' + geo.properties.name,
-			                    ': ' + data.numberOfEvents,
+			            return ['<div class="hoverinfo"><em>',
+			                    data.numberOfEvents + '</em>',
+			                    'events in <strong>' + geo.properties.name,
 			                    '</strong></div>'].join('');
 					},
 			        highlightFillColor: highlightFillColor,
-			        highlightBorderColor: 'rgba(0,0,0,0)',
+			        highlightBorderColor: 'rgba(0,0,0,.5)',
 			        highlightBorderWidth: 1
 		        },
 		        done: function(datamap) {
@@ -66,7 +66,7 @@ app.directive("heatmap", function(utilityService) {
 			        	var index = $.inArray(stateAbbr, scope[attrs.highlightedStates]);
 						if (index == -1) {
 							scope[attrs.highlightedStates].push(stateAbbr);
-							$(this).on("mouseout", function() { 
+							$(this).on("mouseout", function() {
 								$(this).css("fill", fills["highlighted"]);
 			            	});
 						}
