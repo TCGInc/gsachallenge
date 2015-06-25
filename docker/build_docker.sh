8a87043e3b3305a9ff9888957ecf0f75c31aa27e
Robert Buccigrossi <robert.buccigrossi@tcg.com>
2015-06-24 23:16:18 -0400

    Updated build_docker.sh to work with different top-level git repository directory names

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Creating node source ball"
-tar -cf gsac.tar --exclude='../../gsac/docker' ../../gsac/
+tar -cf gsac.tar --transform 's,^,gsac/,' --exclude='../docker' ../*
+echo "Building gsachallenge docker image"
 sudo docker build -t gsachallenge .
