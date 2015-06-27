65fccb4eeac6a043feff98567c6b0791058433eb
Matt Travers <matt.travers@tcg.com>
2015-06-27 15:10:37 -0400

    #108 Added protractor automated tests.

--- tests/protractor-test.js
+++ tests/protractor-test.js
@@ -1,9 +1,95 @@
 describe('GSA Challenge Demo App', function() {
-	it('should have a title', function() {
-		browser.get('http://gsachallenge.tcg.com/');
-		expect(browser.getTitle()).toEqual('GSA Challenge');
+
+	var dashboard = require('../tests/protractor-dashboard.js');
+
+	beforeEach(function() {
+		dashboard.loadPage();
 	});
-	it('should have a clickable link', function() {
-		element(by.linkText('Re-run')).click();
+
+	it('should have the correct page title', function() {
+		expect(dashboard.getTitle()).toEqual('FDA Recall Enforcement Events');
 	});
+
+	it('should be able to add/remove several states by clicking state buttons', function() {
+		dashboard.clickStateOnMap('VA');
+		dashboard.clickStateOnMap('TX');
+		dashboard.clickStateOnMap('MD');
+		var states = dashboard.getHighlightedStates();
+
+		expect(states.count()).toEqual(3);
+		expect(states.first().getText()).toMatch('MD');
+		expect(states.last().getText()).toMatch('VA');
+
+		dashboard.clickStateOnMap('MD');
+		dashboard.clickStateOnMap('VA');
+		states = dashboard.getHighlightedStates();
+
+		expect(states.count()).toEqual(1);
+		expect(states.first().getText()).toMatch('TX');
+	});
+
+	it('should be able to remove a state from the map by clicking the state button above the detail table', function() {
+		dashboard.clickStateOnMap('VA');
+		dashboard.clickStateOnMap('TX');
+		dashboard.clickStateOnMap('MD');
+		var states = dashboard.getHighlightedStates();
+
+		expect(states.count()).toEqual(3);
+		expect(states.first().getText()).toMatch('MD');
+		expect(states.last().getText()).toMatch('VA');
+
+		dashboard.clickStateButton('MD');
+		dashboard.clickStateButton('VA');
+		states = dashboard.getHighlightedStates();
+
+		expect(states.count()).toEqual(1);
+		expect(states.first().getText()).toMatch('TX');
+	});
+
+	it('should be able to search using the search form', function() {
+		var input = element(by.model('searchParams.recallingFirm'));
+		var checkbox = element(by.model('searchParams.eventTypeDevice'));
+		var detailTableCounts = dashboard.getDetailTableCounts();
+
+		input.sendKeys('safeway');
+		checkbox.click();
+
+		expect(detailTableCounts.getText()).toEqual('Showing 1 to 8 of 8 entries');
+	});
+
+	it('clicking the Clear Filters button should clear the form elements and the selected states', function() {
+		var input = element(by.model('searchParams.recallingFirm'));
+		var checkbox = element(by.model('searchParams.eventTypeDevice'));
+		var states = dashboard.getHighlightedStates();
+		var detailTableCounts = dashboard.getDetailTableCounts();
+
+		input.sendKeys('safeway');
+		checkbox.click();
+		dashboard.clickStateOnMap('VA');
+
+		expect(input.getAttribute('value')).toEqual('safeway');
+		expect(checkbox.getAttribute('checked')).toBe(null);
+		expect(detailTableCounts.getText()).toEqual('Showing 1 to 6 of 6 entries');
+		expect(states.count()).toEqual(1);
+
+		dashboard.clickClearFiltersButton();
+
+		expect(input.getAttribute('value')).toEqual('');
+		expect(checkbox.getAttribute('checked')).toBe('true');
+		expect(detailTableCounts.getText()).not.toEqual('Showing 1 to 6 of 6 entries');
+		expect(states.count()).toEqual(0);
+	});
+
+	it('should be able to save a search', function() {
+		var randomString = dashboard.getRandomString(10);
+
+		element(by.className('save_icon')).click();
+		element(by.model('saveSearchName')).sendKeys(randomString);
+		element(by.buttonText('Save')).click();
+
+		var name = element(by.binding('savedSearch.name'));
+
+		expect(name.getText()).toEqual(randomString);
+	});
+
 });
