57cc8041a1ae8a0f3ca84d17770e3e35305d1a34
Matt Travers <matt.travers@tcg.com>
2015-06-30 14:14:56 -0400

    #129 - Added spinner when openning the detail modal from the details table.

--- public/js/directives/recallDetail.js
+++ public/js/directives/recallDetail.js
@@ -1,4 +1,4 @@
-app.directive("recallDetail", function($modal) {
+app.directive("recallDetail", function($modal, $http, $log, utilityService) {
 	return {
 		restrict: "E",
 		templateUrl: "templates/recallDetail.html",
@@ -6,52 +6,60 @@ app.directive("recallDetail", function($modal) {
 
 			scope.openRecallDetailModal = function() {
 
-				scope.eventId = $("span", element).data("event_id");
-				scope.productType = $("span", element).data("product_type");
+				// Open data table processing spinner.
+				$("#DataTables_Table_0_processing").show();
 
-			    $modal.open({
-					animation: true,
-					templateUrl: "recallDetail.html",
-					controller: "recallDetailModalInstanceController",
-					size: "lg",
-					scope: scope
-			    });
+				// Retrive query variables from the HTML element.
+				var eventId = $("span", element).data("event_id");
+				var productType = $("span", element).data("product_type");
+
+				// Reformat product type to meet endpoint specs.
+				if (productType == "devices") productType = "device";
+				if (productType == "drugs") productType = "drug";
+
+				try {
+					$http.get("/fda/recalls/" + productType + "/" + eventId).
+						success(function(data, status, headers, config) {
+							if (data.status.error) {
+								throw data.status.message;
+							}
+
+							function reformatDate(rawDate) {
+								var parts = rawDate.toString().match(/^(\d{4})(\d{2})(\d{2})$/);
+								return parts[2] + "/" + parts[3] + "/" + parts[1];
+							}
+
+							data.result.recall_initiation_date = reformatDate(data.result.recall_initiation_date);
+							data.result.report_date = reformatDate(data.result.report_date);
+
+							scope.details = data.result;
+
+						    $modal.open({
+								animation: true,
+								templateUrl: "recallDetail.html",
+								controller: "recallDetailModalInstanceController",
+								size: "lg",
+								scope: scope
+						    });
+
+							// Close data table processing spinner.
+							$("#DataTables_Table_0_processing").hide();
+						}).
+						error(function(data, status, headers, config) {
+							throw JSON.stringify(data) + JSON.stringify(status);
+						});
+				}
+				catch(errorMessage) {
+					$log.error(errorMessage);
+					utilityService.addAlert($scope.modalAlerts, "danger", "There is a system problem when retrieving the detail.");
+				}
+
 			};
 		}
     }
 });
 
-app.controller("recallDetailModalInstanceController", function ($scope, $http, $log, $modalInstance, utilityService) {
-
-	// Reformat product type to meet endpoint specs.
-	if ($scope.productType == "devices") $scope.productType = "device";
-	if ($scope.productType == "drugs") $scope.productType = "drug";
-
-	try {
-		$http.get("/fda/recalls/" + $scope.productType + "/" + $scope.eventId).
-			success(function(data, status, headers, config) {
-				if (data.status.error) {
-					throw data.status.message;
-				}
-
-				function reformatDate(rawDate) {
-					var parts = rawDate.toString().match(/^(\d{4})(\d{2})(\d{2})$/);
-					return parts[2] + "/" + parts[3] + "/" + parts[1];
-				}
-
-				data.result.recall_initiation_date = reformatDate(data.result.recall_initiation_date);
-				data.result.report_date = reformatDate(data.result.report_date);
-
-				$scope.details = data.result;
-			}).
-			error(function(data, status, headers, config) {
-				throw JSON.stringify(data) + JSON.stringify(status);
-			})
-	}
-	catch(errorMessage) {
-		$log.error(errorMessage);
-		utilityService.addAlert($scope.modalAlerts, "danger", "There is a system problem when retrie.");
-	}
+app.controller("recallDetailModalInstanceController", function ($scope, $modalInstance) {
 
 	$scope.close = function () {
 		$modalInstance.dismiss("cancel");
